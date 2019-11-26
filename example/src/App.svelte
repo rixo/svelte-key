5528c6be5da6d46ea9ada6248d96f0aee55ef3fe
rixo <rixo@rixo.fr>
2019-11-26 12:18:46 +0100

    add example

---
+++ example/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Identity from 'svelte-key'
+	let i = 0
+</script>
+
+<p>Change input value & click button</p>
+
+<Identity key={i}>
+	<input />
+</Identity>
+
+<button on:click={() => {i++}}>
+	{i}
+</button>
